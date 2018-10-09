eaf1ca293fb75e541f7c92b977c7aa34574246b0
Christine Vinaviles <christine.vinaviles@gmail.com>
2018-10-09 19:20:58 +1100

    fix errors

--- youtube_schema.sql
+++ youtube_schema.sql
@@ -62,17 +62,13 @@ CREATE TABLE playlist_items (
 
 /* Insert dummy data */
 
-INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
-    acc_type) VALUES ('ac00001', 'bob@bobbers.com', '04111111115', '11-Jun-2000', 'male',
-    'dvlatoeupofjzdjkvc#R3efijaej', 'manager');
-INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_recovery_email, acc_dob,
-     acc_gender, acc_password, acc_type) VALUES ('ac00002', 'sad@sadders.com', '04111111116',
-     'bop@sadders.com', '16-Dec-1980', 'female', 'ndsfoih4thslakdhg39rgd9gsid9', 'manager');
-INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
-    acc_type, acc_manager_id) VALUES ('ac00003', 'happy@hoppers.com', '04111111117', '11-Feb-2010',
-    'male', '23424cref3ewr323r2fwee32dewf', 'child', 'ac00001');
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password, acc_type)
+    VALUES ('ac00001', 'bob@bobbers.com', '04111111115', '11-Jun-2000', 'male', 'dvlatoeupofjzdjkvc#R3efijaej', 'manager');
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_recovery_email, acc_dob, acc_gender, acc_password, acc_type)
+    VALUES ('ac00002', 'sad@sadders.com', '04111111116', 'bop@sadders.com', '16-Dec-1980', 'female', 'ndsfoih4thslakdhg39rgd9gsid9', 'manager');
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password, acc_type, acc_manager_id) 
+    VALUES ('ac00003', 'happy@hoppers.com', '04111111117', '11-Feb-2010', 'male', '23424cref3ewr323r2fwee32dewf', 'child', 'ac00001');
 
 INSERT INTO channels (channel_id, channel_name, channel_description, channel_created_at, acc_id)
-    VALUES ('ch00002', 'chrisstime', 'Sometimes I play music. Sometimes I am music.', '12-Sep-2015'
-    'ac00001');
+    VALUES ('ch00001', 'chrisstime', 'Sometimes I play music. Sometimes I am music.', '12-Sep-2015', 'ac00001');
     
