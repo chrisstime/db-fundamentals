3379dc99b7565af1776f64d2957afd2c30eae46f
Christine Vinaviles <christine.vinaviles@gmail.com>
2018-10-09 19:02:58 +1100

    add channels and fix up accounts

--- youtube_schema.sql
+++ youtube_schema.sql
@@ -14,11 +14,11 @@ DROP TABLE playlists        CASCADE;
 DROP TABLE playlist_items   CASCADE;
 
 
-CREATE TABLE Accounts (
+CREATE TABLE accounts(
     acc_id                  char(10)            NOT NULL,
     acc_email               varchar(30)         NOT NULL,
     acc_recovery_email      varchar(30),
-    acc_recovery_phone      varchar(20),
+    acc_recovery_phone      varchar(30),
     acc_dob                 date                NOT NULL,
     acc_gender              char(10)            NOT NULL,
     acc_password            varchar(30)         NOT NULL,
@@ -26,6 +26,29 @@ CREATE TABLE Accounts (
     acc_manager_id          char(10), 
     PRIMARY KEY (acc_id)
 );
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
+    acc_type) VALUES ('ac00001', 'bob@bobbers.com', '04111111115', '11-Jun-2000', 'male',
+    'dvlatoeupofjzdjkvc#R3efijaej', 'manager');
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_recovery_email, acc_dob,
+     acc_gender, acc_password, acc_type) VALUES ('ac00002', 'sad@sadders.com', '04111111116',
+     'bop@sadders.com', '16-Dec-1980', 'female', 'ndsfoih4thslakdhg39rgd9gsid9', 'manager');
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
+    acc_type, acc_manager_id) VALUES ('ac00003', 'happy@hoppers.com', '04111111117', '11-Feb-2010',
+    'male', '23424cref3ewr323r2fwee32dewf', 'child', 'ac00001');
+
+CREATE TABLE channels (
+    channel_id            char(10)            NOT NULL,
+    channel_name          varchar(25)         NOT NULL,
+    channel_description   varchar(75),
+    channel_created_at    date                NOT NULL,
+    acc_id                char(10)            NOT NULL,
+    CONSTRAINT channels_fk PRIMARY KEY (channel_id),
+    CONSTRAINT channels_fk FOREIGN KEY (acc_id) REFERENCES accounts(acc_id)
+)
+INSERT INTO channels (channel_id, channel_name, channel_description, channel_created_at, acc_id)
+    VALUES ('ch00002', 'chrisstime', 'Sometimes I play music. Sometimes I am music.', '12-Sep-2015'
+    'ac00001');
+    
 CREATE TABLE playlists (
     playlist_id             varchar(10)         NOT NULL,
     playlist_title          varchar(30),
