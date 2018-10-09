370253a7bef4b8f31cf535d65728df164d471260
Christine Vinaviles <christine.vinaviles@gmail.com>
2018-10-09 21:09:43 +1100

    Add initial queries.sql file

---
+++ queries.sql
@@ -0,0 +1,10 @@
+/** 
+    Part D Database Fundamentals
+    Author: Christine Vinaviles
+    Student Number: 11986282
+    Author: John Pham
+    Student Number: 
+**/
+
+SELECT * FROM accounts;
+SELECT * FROM channels;
