a9cc6c0a8a3dd252c3f924b00dbb3ef4d286fec4
Christine Vinaviles <christine.vinaviles@gmail.com>
2018-10-09 19:04:16 +1100

    remove bad insert

--- youtube_schema.sql
+++ youtube_schema.sql
@@ -71,8 +71,3 @@ CREATE TABLE playlist_items (
   vid_id                    varchar(10)         NOT NULL,
   CONSTRAINT playlist_items_pk PRIMARY KEY (playlist_id, vid_id)
 );
-
-
-INSERT INTO Accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
-    acc_type) VALUES ('ac00001', 'bob@bobbers.com', '04111111116', '11-Jun-1980', 
-    '23424cref3ewr323r2fwee32dewfwer3rfewa', 'male', 'manager');
