b70f52f27ee604b67a3d3ff1dc32434084b7c09a
Christine Vinaviles <christine.vinaviles@gmail.com>
2018-10-09 19:05:21 +1100

    move inserts at the bottom

--- youtube_schema.sql
+++ youtube_schema.sql
@@ -26,16 +26,6 @@ CREATE TABLE accounts(
     acc_manager_id          char(10), 
     PRIMARY KEY (acc_id)
 );
-INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
-    acc_type) VALUES ('ac00001', 'bob@bobbers.com', '04111111115', '11-Jun-2000', 'male',
-    'dvlatoeupofjzdjkvc#R3efijaej', 'manager');
-INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_recovery_email, acc_dob,
-     acc_gender, acc_password, acc_type) VALUES ('ac00002', 'sad@sadders.com', '04111111116',
-     'bop@sadders.com', '16-Dec-1980', 'female', 'ndsfoih4thslakdhg39rgd9gsid9', 'manager');
-INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
-    acc_type, acc_manager_id) VALUES ('ac00003', 'happy@hoppers.com', '04111111117', '11-Feb-2010',
-    'male', '23424cref3ewr323r2fwee32dewf', 'child', 'ac00001');
-
 CREATE TABLE channels (
     channel_id            char(10)            NOT NULL,
     channel_name          varchar(25)         NOT NULL,
@@ -45,10 +35,6 @@ CREATE TABLE channels (
     CONSTRAINT channels_fk PRIMARY KEY (channel_id),
     CONSTRAINT channels_fk FOREIGN KEY (acc_id) REFERENCES accounts(acc_id)
 )
-INSERT INTO channels (channel_id, channel_name, channel_description, channel_created_at, acc_id)
-    VALUES ('ch00002', 'chrisstime', 'Sometimes I play music. Sometimes I am music.', '12-Sep-2015'
-    'ac00001');
-    
 CREATE TABLE playlists (
     playlist_id             varchar(10)         NOT NULL,
     playlist_title          varchar(30),
@@ -71,3 +57,20 @@ CREATE TABLE playlist_items (
   vid_id                    varchar(10)         NOT NULL,
   CONSTRAINT playlist_items_pk PRIMARY KEY (playlist_id, vid_id)
 );
+
+/* Insert dummy data */
+
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
+    acc_type) VALUES ('ac00001', 'bob@bobbers.com', '04111111115', '11-Jun-2000', 'male',
+    'dvlatoeupofjzdjkvc#R3efijaej', 'manager');
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_recovery_email, acc_dob,
+     acc_gender, acc_password, acc_type) VALUES ('ac00002', 'sad@sadders.com', '04111111116',
+     'bop@sadders.com', '16-Dec-1980', 'female', 'ndsfoih4thslakdhg39rgd9gsid9', 'manager');
+INSERT INTO accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
+    acc_type, acc_manager_id) VALUES ('ac00003', 'happy@hoppers.com', '04111111117', '11-Feb-2010',
+    'male', '23424cref3ewr323r2fwee32dewf', 'child', 'ac00001');
+
+INSERT INTO channels (channel_id, channel_name, channel_description, channel_created_at, acc_id)
+    VALUES ('ch00002', 'chrisstime', 'Sometimes I play music. Sometimes I am music.', '12-Sep-2015'
+    'ac00001');
+    
