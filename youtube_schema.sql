a8b3ea8104b67f8909cefd2cbbf30b8320697ba9
John Pham <john@s1t2.com.au>
2018-10-09 18:59:58 +1100

    Added playlists, videos, playlist_items

--- youtube_schema.sql
+++ youtube_schema.sql
@@ -4,14 +4,14 @@
     Student Number: 
 **/
 
-DROP TABLE accounts;
-DROP TABLE channels;
-DROP TABLE memberships;
-DROP TABLE membership_types;
-DROP TABLE invoices;
-DROP TABLE videos;
-DROP TABLE playlists;
-DROP TABLE plalist_items;
+DROP TABLE accounts         CASCADE;
+DROP TABLE channels         CASCADE;
+DROP TABLE memberships      CASCADE;
+DROP TABLE membership_types CASCADE;
+DROP TABLE invoices         CASCADE;
+DROP TABLE videos           CASCADE;
+DROP TABLE playlists        CASCADE;
+DROP TABLE playlist_items   CASCADE;
 
 
 CREATE TABLE Accounts(
@@ -26,6 +26,30 @@ CREATE TABLE Accounts(
     acc_manager_id          char(10), 
     PRIMARY KEY (acc_id)
 );
+CREATE TABLE playlists(
+    playlist_id             varchar(10)         NOT NULL,
+    playlist_title          varchar(30),
+    playlist_description    varchar(255),
+    CONSTRAINT playlist_pk PRIMARY KEY (playlist_id),
+    CONSTRAINT playlist_fk1 FOREIGN KEY (channel_id) REFERENCES channels(channel_id)
+);
+CREATE TABLE videos(
+  vid_id                    varchar(10),
+  vid_title                 varchar(20),
+  vid_description           varchar(255),
+  vid_message               varchar(255),
+  vid_privacy_setting       varchar(255),
+  channel_id                varchar(10),
+  CONSTRAINT videos_pk PRIMARY KEY (vid_id),
+  CONSTRAINT videos_fk1 FOREIGN KEY (channel_id) REFERENCES channels(channel_id)
+);
+CREATE TABLE playlist_items(
+  playlist_item_position    int                 NOT NULL,
+  playlist_id               varchar(10)         NOT NULL,
+  vid_id                    varchar(10)         NOT NULL,
+  CONSTRAINT playlist_items_pk PRIMARY KEY (playlist_id, vid_id)
+);
+
 
 INSERT INTO Accounts (acc_id, acc_email, acc_recovery_phone, acc_dob, acc_gender, acc_password,
     acc_type) VALUES ('ac00001', 'bob@bobbers.com', '04111111116', '11-Jun-1980', 
